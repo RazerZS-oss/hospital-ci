443facd147fd26f1e091c329683bec92ce7e85dd
royhan azhar <roihanazhar24@gmail.com>
2026-09-19 21:29:34 +0700

    chore(ci3): remove leftover CI4 files and add CI3 IDE helper

---
+++ _ide_helper.php
@@ -0,0 +1,72 @@
+<?php
+/**
+ * IDE Helper for CodeIgniter 3 & Intelephense
+ * 
+ * This file is purely for IDE autocompletion and static analysis.
+ * It is never executed at runtime.
+ */
+
+namespace {
+    /**
+     * @property CI_Benchmark        $benchmark
+     * @property CI_Calendar         $calendar
+     * @property CI_Cart             $cart
+     * @property CI_Config           $config
+     * @property CI_Controller       $controller
+     * @property CI_Email            $email
+     * @property CI_Encrypt          $encrypt
+     * @property CI_Encryption       $encryption
+     * @property CI_Upload           $upload
+     * @property CI_Form_validation  $form_validation
+     * @property CI_FTP              $ftp
+     * @property CI_Hooks            $hooks
+     * @property CI_Image_lib        $image_lib
+     * @property CI_Input            $input
+     * @property CI_Javascript       $javascript
+     * @property CI_Lang             $lang
+     * @property CI_Loader           $load
+     * @property CI_Log              $log
+     * @property CI_Migration        $migration
+     * @property CI_Output           $output
+     * @property CI_Pagination       $pagination
+     * @property CI_Parser           $parser
+     * @property CI_Profiler         $profiler
+     * @property CI_Router           $router
+     * @property CI_Security         $security
+     * @property CI_Session          $session
+     * @property CI_Table            $table
+     * @property CI_Trackback        $trackback
+     * @property CI_Typography       $typography
+     * @property CI_Unit_test        $unit_test
+     * @property CI_URI              $uri
+     * @property CI_User_agent       $user_agent
+     * @property CI_Xmlrpc           $xmlrpc
+     * @property CI_Xmlrpcs          $xmlrpcs
+     * @property CI_Zip              $zip
+     * @property CI_DB_query_builder $db
+     * @property CI_DB_forge         $dbforge
+     * @property CI_DB_utility       $dbutil
+     * @property M_user              $M_user
+     * @property M_pasien            $M_pasien
+     * @property M_dokter            $M_dokter
+     */
+    class CI_Controller {}
+
+    /**
+     * @property CI_Benchmark        $benchmark
+     * @property CI_Config           $config
+     * @property CI_DB_query_builder $db
+     * @property CI_DB_forge         $dbforge
+     * @property CI_DB_utility       $dbutil
+     * @property CI_Input            $input
+     * @property CI_Loader           $load
+     * @property CI_Security         $security
+     * @property CI_Session          $session
+     * @property CI_URI              $uri
+     * @property CI_Lang             $lang
+     * @property M_user              $M_user
+     * @property M_pasien            $M_pasien
+     * @property M_dokter            $M_dokter
+     */
+    class CI_Model {}
+}
